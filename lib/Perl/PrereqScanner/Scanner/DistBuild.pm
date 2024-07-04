0d120d09cffcf554ab4713659406a1205546ebee
Leon Timmermans <fawaka@gmail.com>
2024-07-04 19:50:00 +0200

    Initial commit

---
+++ lib/Perl/PrereqScanner/Scanner/DistBuild.pm
@@ -0,0 +1,35 @@
+package Perl::PrereqScanner::Scanner::DistBuild;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.000';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Perl::PrereqScanner::Scanner::DistBuild - INSERT YOUR ABSTRACT HERE
+
+=head1 DESCRIPTION
+
+Write a full description of the module and its features here.
+
+=head1 AUTHOR
+
+Leon Timmermans <fawaka@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2024 by Leon Timmermans.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
